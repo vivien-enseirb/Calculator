41173688d6e545fbaab57f634dac6bb99a646e7c
Vivien <vnebout@enseirb-matmeca.fr>
2025-10-15 18:16:10 +0200

    [IMPLEMENTING] Evaluateur.cs Added an Evaluator in order to evaluate a tree

---
+++ Calculator/Services/Evaluateur/Evaluateur.cs
@@ -0,0 +1,26 @@
+﻿namespace Calculator.Services.Evaluateur;
+using Calculator.Services.MathUtils;
+using Calculator.Services.Token;
+using Calculator.Services.Node;
+
+/// <summary>
+/// Définit l'évaluation d'un noeud
+/// </summary>
+public class Evaluateur
+{
+    public int Evaluate(Node node)=>
+        node switch
+        {
+            NumberNode n => n.Value,
+            BinaryNode(var Left, TokenType.Plus, Node Right) => Services.MathUtils.MathUtils.Add(Evaluate(Left),
+                Evaluate(Right)),
+            BinaryNode(var Left, TokenType.Minus, Node Right) => Services.MathUtils.MathUtils.Substract(Evaluate(Left),
+                Evaluate(Right)),
+            BinaryNode(var Left, TokenType.Multiply, Node Right) => Services.MathUtils.MathUtils.Multiply(
+                Evaluate(Right), Evaluate(Left)),
+            BinaryNode(var Left, TokenType.Divide, Node Right) => Services.MathUtils.MathUtils.Divide(Evaluate(Right),
+                Evaluate(Left)),
+            _ => throw new InvalidOperationException($"Noeud non supporté : {node}")
+        };
+    
+}
