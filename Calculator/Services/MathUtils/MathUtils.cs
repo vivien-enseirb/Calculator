42989390791c6fe1e0189beef7291aac6bced406
Vivien <vnebout@enseirb-matmeca.fr>
2025-10-15 18:12:53 +0200

    [IMPLEMENTING] MathUtils.cs Implemented some basic operators

---
+++ Calculator/Services/MathUtils/MathUtils.cs
@@ -0,0 +1,28 @@
+﻿namespace Calculator.Services.MathUtils;
+
+public class MathUtils
+{
+    public static int Add(int a, int b)
+    {
+        return  a + b;
+    }
+
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Ty es un golmon tu divises par zero");
+        }
+        return a / b;
+    }
+}
