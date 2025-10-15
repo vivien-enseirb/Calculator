a3eebd232f12102250b059d944e6c48f4042dfba
Vivien <vnebout@enseirb-matmeca.fr>
2025-10-15 18:16:54 +0200

    [IMPLEMENTING] Calculatrice.cs Implemented a calculator

---
+++ Calculator/Services/Calculatrice/Calculatrice.cs
@@ -0,0 +1,31 @@
+﻿namespace Calculator.Services.Calculatrice;
+using Calculator.Services.AnalyseurSyntaxique;
+using Calculator.Services.Evaluateur;
+using Calculator.Services.Token;
+using Calculator.Services.Node;
+public class Calculatrice
+{
+    public int Calcul(string expression)
+    {
+        try
+        {
+            List<Token> tokens = Tokenizer.Tokenize(expression);
+            AnalyseurSyntaxique analyseurSyntaxique = new AnalyseurSyntaxique(tokens);
+
+            if (!analyseurSyntaxique.EstBienParenthese(tokens))
+                throw new Exception("L'expression est mal parenthésée");
+
+            Node node = analyseurSyntaxique.AnalyseExpression();
+            Evaluateur evaluateur = new Evaluateur();
+            int resultat = evaluateur.Evaluate(node);
+            return resultat;   
+        }
+        catch (Exception ex)
+        {
+            // Affiche ou log l'erreur, utile pour le débogage
+            Console.WriteLine("Erreur : " + ex.Message);
+            throw;
+        }
+    }
+
+}
